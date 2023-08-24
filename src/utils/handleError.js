8b0ff29f1066f6ade364a271c287630db490b2e0
e-lamine <lamine@e-lamine.dev>
2023-08-24 10:56:50 +0100

    show only error message

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,10 +1,10 @@
 export default function handleError(error) {
    const searchInput = document.querySelector('.search-input');
    if (searchInput) {
-      searchInput.setCustomValidity(error);
+      searchInput.setCustomValidity(error.message);
       searchInput.reportValidity();
       searchInput.setCustomValidity('');
    } else {
-      document.querySelector('.loading-msg').textContent = error;
+      document.querySelector('.loading-msg').textContent = error.message;
    }
 }
